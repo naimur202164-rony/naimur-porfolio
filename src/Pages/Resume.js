1fc8fd419d3055f7fb3e8f9a0220a1713b03f413
unknown <you@example.com>
2022-01-26 23:48:06 +0600

    Resume added

--- src/Pages/Resume.js
+++ src/Pages/Resume.js
@@ -1,13 +1,11 @@
-import React from 'react'
-import { Container, Row} from "react-bootstrap";
+import React from 'react';
+import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
-import Particle from "../Components/Particles/Particle";
- import resume from "../Assets/resume1.png";
- import project from "../Assets/project-his.png";
- import pdf from "../Assets/resume.pdf"
-
-
 import { AiOutlineDownload } from "react-icons/ai";
+import pdf from "../Assets/resume.pdf";
+import Particle from "../Components/Particles/Particle";
+
+
 
 
 function Resume() {
@@ -15,8 +13,8 @@ function Resume() {
         <Container fluid className="resume-section">
         <Particle />
         
-      <img src={resume} alt="error" style={{width:"70%" , marginLeft:"15%", marginBottom:"5%" }}/>
-      <img src={project} alt="error" style={{width:"70%" , marginLeft:"15%" ,marginBottom:"5%" }}/>
+      {/* <img src={resume} alt="error" style={{width:"70%" , marginLeft:"15%", marginBottom:"5%" }}/>
+      <img src={project} alt="error" style={{width:"70%" , marginLeft:"15%" ,marginBottom:"5%" }}/> */}
       
         <Container>
           <Row style={{ justifyContent: "center", position: "relative"}}>
