5e4b30b6ea8a84dbda39c5a4b295f401f208ecd1
naimur <naimur202163@gmail.com>
2023-04-01 10:52:31 +0600

    about section added

--- src/Components/AboutCard/AboutCard.js
+++ src/Components/AboutCard/AboutCard.js
@@ -1,27 +1,29 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 
-
 function AboutCard() {
   return (
     <Card className="quote-card-view">
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, My Self <span className="purple">Mohammad Naimur Rahman </span>
-            I am a English Horns Student from <span className="purple"> Cumilla, Bangladesh  .</span>
+            Hi Everyone, My Self{" "}
+            <span className="purple">Mohammad Naimur Rahman </span>I am a
+            English Horns Student from{" "}
+            <span className="purple"> Cumilla, Bangladesh .</span>
             <br />
-            
-            <br />I have completed  My College in  Science  Group from Jafarganj Mir Abdul Gafur College<br /> <br />
-           <br />
-            
-           <br />
+            <br />I have completed My College Group from Jafarganj Mir Abdul
+            Gafur College
+            <br /> <br />
             <br />
-            ⚡ Apart from this, I have done courses on  Full Stack Development .
- 
+            now i am studying ba english honours Nimsar Junab Ali College <br />{" "}
+            <br />
+            <br />
+            <br />
+            <br />⚡ Apart from this, I have done courses on Content Writing and
+            copywriting .
           </p>
-          
-              </blockquote>
+        </blockquote>
       </Card.Body>
     </Card>
   );
